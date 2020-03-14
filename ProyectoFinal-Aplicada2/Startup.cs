97e7021f47b8be10e69edc6ffb9e2eac170beda7
ezequiel huma brito <ezequielhuma@hotmail.com>
2020-03-14 17:11:26 -0400

    Agregando el proyecto de Test

--- ProyectoFinal-Aplicada2/Startup.cs
+++ ProyectoFinal-Aplicada2/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using ProyectoFinal_Aplicada2.Data;
+using ProyectoFinal_Aplicada2.Controllers;
 
 namespace ProyectoFinal_Aplicada2
 {
@@ -28,7 +29,7 @@ namespace ProyectoFinal_Aplicada2
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
+            services.AddScoped<ProductosControllers>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
