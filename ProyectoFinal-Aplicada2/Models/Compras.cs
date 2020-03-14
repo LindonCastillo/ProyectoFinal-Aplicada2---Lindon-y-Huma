bfc3c20b19d4a3553fda5040cf6659fa6beaef2d
ezequiel huma brito <ezequielhuma@hotmail.com>
2020-03-14 16:00:50 -0400

    Agregando la clase Compra con su Compra-Detalle

---
+++ ProyectoFinal-Aplicada2/Models/Compras.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+
+namespace ProyectoFinal_Aplicada2.Models
+{
+    public class Compras
+    {
+        [Key]
+        [Required(ErrorMessage ="El campo Id debe ser un numero")]
+        [Range(0,100000000,ErrorMessage ="El campo Id no puede ser menor que cero")]
+        public int ComprasId { get; set; }
+        [DataType(DataType.DateTime)]
+        [Required(ErrorMessage ="El campo fecha no puede estar vacio")]
+        public DateTime FechaCompra { get; set; }
+        [Required(ErrorMessage ="El campo ProveedorId debe ser un numero")]
+        [Range(1,1000000000,ErrorMessage ="El campo ProveedorId debe ser mayor que cero")]
+        public int ProveedorId { get; set; }
+        
+        public decimal Itebis { get; set; }
+        public decimal Total { get; set; }
+        public decimal Balance { get; set; }
+
+        public Compras()
+        {
+            ComprasId = 0;
+            FechaCompra = DateTime.Now;
+            ProveedorId = 0;
+            Itebis = 0;
+            Total = 0;
+            Balance = 0;
+        }
+    }
+}
---
+++ ProyectoFinal-Aplicada2/Models/ComprasDetalle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoFinal_Aplicada2.Models
+{
+    public class ComprasDetalle
+    {
+        [Key]
+        public int CompraDetalleId { get; set; }
+        public int CompraId { get; set; }
+        public int ProductoId { get; set; }
+        public string Nombre { get; set; }
+        public int Cantidad { get; set; }
+        public decimal PrecioCompra { get; set; }
+        public decimal SubTotal { get; set; }
+
+        public ComprasDetalle()
+        {
+            CompraDetalleId = 0;
+            CompraId = 0;
+            Nombre = string.Empty;
+            ProductoId = 0;
+            Cantidad = 0;
+            PrecioCompra = 0;
+            SubTotal = 0;
+        }
+    }
+}
