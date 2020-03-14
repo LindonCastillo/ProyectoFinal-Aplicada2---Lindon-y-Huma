e4d628b3c73c2dee083a71cc7c3b03aa15f8c6a0
ezequiel huma brito <ezequielhuma@hotmail.com>
2020-03-14 17:34:51 -0400

    Agregando las migraciones y actualizando la base de datos

--- ProyectoFinal-Aplicada2/Data/Contexto.cs
+++ ProyectoFinal-Aplicada2/Data/Contexto.cs
@@ -11,7 +11,7 @@ namespace ProyectoFinal_Aplicada2.Data
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            base.OnConfiguring(optionsBuilder.UseSqlite("Data Source = "));
+            base.OnConfiguring(optionsBuilder.UseSqlite("Data Source = Database/ProyectoFinalDatabase"));
         }
 
         public DbSet<Productos> Productos { get; set; }
---
+++ ProyectoFinalTests/ProductosTest.cs
@@ -0,0 +1,34 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProyectoFinal_Aplicada2.Controllers;
+using ProyectoFinal_Aplicada2.Models;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using ProyectoFinal_Aplicada2.Data;
+using System.Threading.Tasks;
+using System;
+
+namespace ProyectoFinalTests
+{
+    [TestClass]
+    public class ProductosTest
+    {
+        [TestMethod]
+        public void Guardar()
+        {
+            Productos productos = new Productos();
+            ProductosControllers productosControllers = new ProductosControllers();
+
+            productos.ProductoId = 1;
+            productos.FechaEntrada = DateTime.Now;
+            productos.Nombre = "Alguien";
+            productos.PorcentajeGanancias = 12;
+            productos.PrecioVentas = 2000;
+            productos.Cantidad = 102;
+            productos.PrecioVentas = 120;
+
+            productosControllers.Guardar(productos);
+
+        }
+    }
+}
