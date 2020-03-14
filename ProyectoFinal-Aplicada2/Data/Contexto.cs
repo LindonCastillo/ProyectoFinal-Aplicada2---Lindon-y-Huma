2204acdc0039e287edc397bce485cb04cdb51e11
ezequiel huma brito <ezequielhuma@hotmail.com>
2020-03-14 16:05:49 -0400

    Agregando la clase Compras con su Compra-Detalle

---
+++ ProyectoFinal-Aplicada2/Data/Contexto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProyectoFinal_Aplicada2.Models;
+
+namespace ProyectoFinal_Aplicada2.Data
+{
+    public class Contexto : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder.UseSqlite("Data Source = "));
+        }
+    }
+}
