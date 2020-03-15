33004cfcfb46284e484451c42b76ccf0ad343d2e
ezequiel huma brito <ezequielhuma@hotmail.com>
2020-03-15 14:52:29 -0400

    Actualizando la base de datos

--- ProyectoFinal-Aplicada2/Migrations/20200315185022_InitialCommit2.Designer.cs
+++ ProyectoFinal-Aplicada2/Migrations/20200315185022_InitialCommit2.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProyectoFinal_Aplicada2.Data;
 
 namespace ProyectoFinal_Aplicada2.Migrations
 {
     [DbContext(typeof(Contexto))]
-    partial class ContextoModelSnapshot : ModelSnapshot
+    [Migration("20200315185022_InitialCommit2")]
+    partial class InitialCommit2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
