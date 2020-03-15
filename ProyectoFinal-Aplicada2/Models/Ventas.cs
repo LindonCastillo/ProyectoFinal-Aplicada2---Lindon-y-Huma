b3c4cbe14c5942888503bb93836012ee05e38e3c
ezequiel huma brito <ezequielhuma@hotmail.com>
2020-03-15 16:15:20 -0400

    Agregando un Text muted

--- ProyectoFinal-Aplicada2/Models/Ventas.cs
+++ ProyectoFinal-Aplicada2/Models/Ventas.cs
@@ -27,7 +27,7 @@ namespace ProyectoFinal_Aplicada2.Models
             VentaId = 0;
             FechaVenta = DateTime.Now;
             Total = 0;
-
+            Balance = 0;
             VentasDetalles = new List<VentasDetalle>();
         }
     }
