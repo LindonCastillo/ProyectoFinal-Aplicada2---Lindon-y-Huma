c7ede317c438fb7e6eb19dd26800a06b086adc36
ezequiel huma brito <ezequielhuma@hotmail.com>
2020-03-16 21:58:56 -0400

    Aun trabajando con la paginacion

---
+++ ProyectoFinal-Aplicada2/Pages/Paginacion.razor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Components;
+using System.Threading.Tasks;
+using ProyectoFinal_Aplicada2.Paginacion;
+
+namespace ProyectoFinal_Aplicada2.Pages
+{
+    public class PaginacionBase : ComponentBase
+    {
+        [Parameter]
+        public PagedResultBase Result { get; set; }
+
+        [Parameter]
+        public Action<int> PageChanged { get; set; }
+
+        protected int StartIndex { get; private set; } = 0;
+        protected int FinishIndex { get; private set; } = 0;
+
+        protected override void OnParametersSet()
+        {
+            StartIndex = Math.Max(Result.CurrentPage - 5, 1);
+            FinishIndex = Math.Min(Result.CurrentPage + 5, Result.PageCount);
+
+            base.OnParametersSet();
+        }
+
+        protected void PagerButtonClicked(int page)
+        {
+            PageChanged?.Invoke(page);
+        }
+    }
+}
+
---
+++ ProyectoFinal-Aplicada2/Paginacion/PageResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoFinal_Aplicada2.Paginacion
+{
+    public class PageResult<T> : PagedResultBase where T: class
+    {
+        public IList<T> Results { get; set; }
+
+        public PageResult()
+        {
+            Results = new List<T>();
+        }
+    }
+}
