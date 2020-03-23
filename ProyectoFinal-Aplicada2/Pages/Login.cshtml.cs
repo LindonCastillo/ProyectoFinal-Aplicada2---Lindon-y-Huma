b04bdcac1ffcc17de7625799cd1d8aae6c14e6f6
ezequiel huma brito <ezequielhuma@hotmail.com>
2020-03-23 16:48:37 -0400

    Agregando la ventana del login

---
+++ ProyectoFinal-Aplicada2/Pages/Login.cshtml.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ProyectoFinal_Aplicada2.Pages
+{
+    public class LoginModel : PageModel
+    {
+
+        public string ReturnUrl { get; set; }
+
+
+
+        public async Task<ActionResult> OnGetAsync(string paramUsername, string paramPawoord)
+        {
+            string ReturnUrl = Url.Content("~/");
+
+            try
+            {
+                await HttpContext
+                    .SignOutAsync(
+                    CookieAuthenticationDefaults.AuthenticationScheme);
+            }
+            catch
+            {   }
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, paramUsername),
+                new Claim(ClaimTypes.Role, "Administrator"),
+            };
+            var claimsIdentity = new ClaimsIdentity(
+                claims, CookieAuthenticationDefaults.AuthenticationScheme);
+            var authProperties = new AuthenticationProperties
+            {
+                IsPersistent = true,
+                RedirectUri = this.Request.Host.Value
+            };
+            try
+            {
+                await HttpContext.SignInAsync(
+                CookieAuthenticationDefaults.AuthenticationScheme,
+                new ClaimsPrincipal(claimsIdentity),
+                authProperties);
+            }
+            catch (Exception ex)
+            {
+                string error = ex.Message;
+            }
+            return LocalRedirect(ReturnUrl);
+        }
+    }
+}
