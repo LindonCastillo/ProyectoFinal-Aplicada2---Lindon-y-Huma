eb58aaa1170e2c3aa030d30c38b766f4d4c41e33
ezequiel huma brito <ezequielhuma@hotmail.com>
2020-03-30 12:00:51 -0400

    Trabajando con el boton de buscar

--- ProyectoFinal-Aplicada2Tests1/Controllers/ProductosControllersTests.cs
+++ ProyectoFinal-Aplicada2Tests1/Controllers/ProductosControllersTests.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ProyectoFinal_Aplicada2.Controllers;
 using System;
+using ProyectoFinal_Aplicada2.Models;
 using System.Collections.Generic;
 using System.Text;
 
@@ -12,7 +13,40 @@ namespace ProyectoFinal_Aplicada2.Controllers.Tests
         [TestMethod()]
         public void GuardarTest()
         {
+            Productos productos = new Productos();
+            productos.ProductoId = 0;
+            productos.Nombre = "Ezequiel";
+            productos.FechaEntrada = DateTime.Now;
+            productos.Cantidad = 0;
+            productos.PrecioCompra = 12;
+            productos.PrecioVentas = 12;
+            productos.PorcentajeGanancias = 12;
+            ProductosControllers productosControllers = new ProductosControllers();
+            Assert.IsTrue(productosControllers.Guardar(productos));
+        }
 
+        [TestMethod()]
+        public void GuardarTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EliminarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void BuscarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetListTest()
+        {
+            Assert.Fail();
         }
     }
 }
