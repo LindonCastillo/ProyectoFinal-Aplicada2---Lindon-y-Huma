cac1e615eedc50fac10bb077f81f53633c6a430b
Lindon Emil Castillo Burgos <lindon_castillo@ucne.edu.do>
2020-03-30 21:53:19 -0400

    Controllers y models de clientes y categorias

---
+++ ProyectoFinal-Aplicada2/Controllers/ClientesControllers.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProyectoFinal_Aplicada2.Data;
+using ProyectoFinal_Aplicada2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace ProyectoFinal_Aplicada2.Controllers
+{
+    public class ClientesControllers
+    {
+        public bool Guardar(Clientes clientes)
+        {
+            bool paso = false;
+            Contexto db = new Contexto();
+            try
+            {
+                if (db.Clientes.Any(A => A.ClientesId == clientes.ClientesId))
+                {
+                    paso = Modificar(clientes);
+                }
+                else
+                {
+                    paso = Insertar(clientes);
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return paso;
+        }
+
+
+        private bool Insertar(Clientes clientes)
+        {
+            Contexto db = new Contexto();
+            bool paso = false;
+
+            try
+            {
+                db.Clientes.Add(clientes);
+                paso = db.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return paso;
+        }
+
+        private bool Modificar(Clientes clientes)
+        {
+            bool paso = false;
+            Contexto db = new Contexto();
+            try
+            {
+                db.Entry(clientes).State = EntityState.Modified;
+                paso = db.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return paso;
+        }
+
+        public bool Eliminar(int id)
+        {
+            bool paso = false;
+            Contexto db = new Contexto();
+
+            try
+            {
+                Clientes clientes = db.Clientes.Find(id);
+                if (clientes != null)
+                {
+                    db.Entry(clientes).State = EntityState.Deleted;
+                    paso = db.SaveChanges() > 0;
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return paso;
+        }
+
+        public Clientes Buscar(int id)
+        {
+            Clientes cliente;
+            Contexto db = new Contexto();
+            try
+            {
+                cliente = db.Clientes.Find(id);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return cliente;
+        }
+
+        public List<Clientes> GetList(Expression<Func<Clientes, bool>> expression)
+        {
+            Contexto db = new Contexto();
+            List<Clientes> lista = new List<Clientes>();
+            try
+            {
+                double conteo = lista.Count();
+                lista = db.Clientes.Where(expression).ToList();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return lista;
+        }
+
+    }
+}
---
+++ ProyectoFinal-Aplicada2/Models/Clientes.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoFinal_Aplicada2.Models
+{
+    public class Clientes
+    {
+        [Key]
+        [Range(0, 1000000000, ErrorMessage = "El campo Id no puede ser menor que cero")]
+        [Required(ErrorMessage = "El campo Id debe ser un numero")]
+        public int ClientesId { get; set; }
+        [Required(ErrorMessage = "El nombre no puede estar vacío")]
+        [MinLength(2, ErrorMessage = "El nombre es muy corto")]
+        [MaxLength(40, ErrorMessage = "Nombre muy largo")]
+        public string Nombre { get; set; }
+        [Required(ErrorMessage = "El apellido no puede estar vacío")]
+        [MinLength(2, ErrorMessage = "El apellido es muy corto")]
+        [MaxLength(40, ErrorMessage = "Apellido muy largo")]
+        public string Apellido { get; set; }
+        [Required(ErrorMessage = "El campo dirección no puede estar vacío")]
+        [MinLength(4, ErrorMessage = "El campo dirección es muy corto")]
+        [MaxLength(60, ErrorMessage = "El campo dirección es muy corto")]
+        public string Dirreccion { get; set; }
+
+        [Required(ErrorMessage = "El campo Número no puede estar vacío")]
+        [MaxLength(10, ErrorMessage = "El campo Número no tiene mas de diez digitos")]
+        [MinLength(10, ErrorMessage = "El campo Número no puede tener menos de diez digitos")]
+        [RegularExpression("^(?!0+$)(\\+\\d{1,3}[- ]?)?(?!0+$)\\d{10,15}$", ErrorMessage = "Por favor ingrese un Número de contacto valido")]
+        public string Numero { get; set; }
+
+        public Clientes()
+        {
+            ClientesId = 0;
+            Nombre = string.Empty;
+            Apellido = string.Empty; 
+            Numero = string.Empty;
+            Dirreccion = string.Empty;
+        }
+    }
+}
